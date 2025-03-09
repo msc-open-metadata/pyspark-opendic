b50f8125bb016cb0688143620fce2f5ab45f3189
Carl Bruun <carl99@live.dk>
2025-03-09 15:43:20 +0100

    feat: - Added catalog class for pyspark library.    - includes fallback to default spark catalog if query does not match OpenDict syntax. - Defined generalized reggex parsing for CREATE and SHOW of OpenDict objects.

--- src/pyspark_opendic/catalog.py
+++ src/pyspark_opendic/catalog.py
@@ -1,2 +1,56 @@
-def main() -> None:
-    print("Hello from pyspark-opendic!")
+from pyspark.sql.catalog import Catalog
+from pyspark.sql import SparkSession
+import re
+import requests
+
+class OpenDicCatalog(Catalog):
+    def __init__(self, sparkSession, api_url):
+        self.api_url = api_url
+        self.sparkSession = sparkSession
+        #TODO: add handling for credentials for client OAuth
+
+    def sql(self, sqlText : str):
+        query_cleaned = sqlText.strip()
+
+        # TODO: do some systematic syntax union - include alias 'as', etc.
+        # TODO: add support for 'or replace' and 'temporary' keywords etc. on catalog-side
+        # Syntax: CREATE [OR REPLACE] [TEMPORARY] OPEN <object_type> <name> [IF NOT EXISTS] [AS <alias>] <arguments>
+        opendic_create_pattern = (
+            r"^create"                             # "create" at the start
+            r"(?:\s+or\s+replace)?"                 # Optional "or replace"
+            r"(?:\s+temporary)?"                    # Optional "temporary"
+            r"\s+open\s+(?P<object_type>\w+)"       # Required object type after "open"
+            r"\s+(?P<name>\w+)"                     # Required name of the object
+            r"(?:\s+if\s+not\s+exists)?"            # Optional "if not exists"
+            r"(?:\s+as\s+(?P<alias>\w+))?"          # Optional alias after "as"
+            r"(?:\s+(?P<arguments>.*))?"            # Remaining arguments
+        )
+
+        # TODO: Add pattern match for Show, Describe, Drop, etc.
+
+        opendic_show_pattern = (
+            r"^show"                                # "show" at the start
+            r"\s+open\s+(?P<object_type>\w+)"        # Required object type after "open"
+            r"s?"                                    # Optionally match a trailing "s"
+        )
+
+        # Check pattern matches
+        create_match = re.match(opendic_create_pattern, query_cleaned, re.IGNORECASE)
+        show_match = re.match(opendic_show_pattern, query_cleaned, re.IGNORECASE)
+
+
+        if create_match:
+            object_type = create_match.group('object_type')
+            name = create_match.group('name')
+            alias = create_match.group('alias')
+            arguments = create_match.group('arguments')  
+
+            # TODO: Initialize openapi specific component e.g. the payload
+            # TODO: Call REST client to create object
+        elif show_match:
+            object_type = show_match.group('object_type')
+            # TODO: Call REST client to show object
+
+        # Fallback to Spark parser
+        return self.spark.sql(query_cleaned)
+
